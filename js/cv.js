ed237b77c471eba9d47c2d8dc42e67e96cdeb73a
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2019-10-17 23:59:39 +0700

    Commit 3/3: Dynamic Resume Page

--- js/cv.js
+++ js/cv.js
@@ -6,7 +6,10 @@ function imageEnlarge(elem) {
   let img = elem.querySelector('img')
   img.classList.remove("inlineLogo")
   img.classList.add("inlineLogoEnlarge")
+  let trash = elem.querySelector('i.fa-trash')
+  trash.style.display = 'unset';
 }
+
 function imageReset(elem) {
   let divs = elem.querySelectorAll('div')
   divs.forEach((div) => {
@@ -15,4 +18,68 @@ function imageReset(elem) {
   let img = elem.querySelector('img')
   img.classList.remove("inlineLogoEnlarge")
   img.classList.add("inlineLogo")
+  let trash = elem.querySelector('i.fa-trash')
+  trash.style.display = 'none';
+}
+
+function promptEditField(mode = "name") {
+  let name = prompt(mode === "name" ? "กรุณาใส่ชื่อ-นามสกุล" : "กรุณาใส่ชื่อเล่น")
+  document.getElementById(mode === "name" ? "myName" : "myNickname").innerText = name
+}
+
+function addSkill() {
+  let name = prompt("กรุณากรอกชื่อความสามารถ")
+  if (!name) {
+    alert("กรุณากรอกชื่อความสามารถ!")
+    return;
+  }
+
+  let level = prompt("กรุณากรอกระดับคะแนน")
+  if (!level || isNaN(level) || level < 0.0 || level > 5.0) {
+    alert("กรุณากรอกระดับคะแนน ที่มีค่าตั้งแต่ 0-5 (สามารถใส่ค่า 0.5 ได้)!")
+    return;
+  }
+
+  let children = $("#skillTable").children()
+  let childrenLength = children.length
+
+  let stars = []
+
+  for (let index = 0; index < Math.floor(level); index++) {
+    stars.push(`<i class="fas fa-star"></i>`)
+  }
+  
+  let isHalf = level % 1 === 0.5
+  if (isHalf) {
+    stars.push(`<i class="fas fa-star-half-alt"></i>`)
+  }
+
+  let remainStars = (5 - stars.length)
+  for (let index = 0; index < remainStars; index++) {
+    stars.push(`<i class="far fa-star"></i>`)
+  }
+
+  let mockElem = `<div class="table-row justifyCenter" onmouseover="imageEnlarge(this)"
+    onmouseout="imageReset(this)">
+    <div class="serial">` + (childrenLength - 1) + `</div>
+    <div class="skill"> <img src="img/logo-programming/devicon.svg" class="inlineLogo"
+    alt="` + name + `">` + name + `<i class="fas fa-trash clickable pl-2" style="display: none; color: red;" onclick="removeSkill(this)"></i></div>
+    <div class="rating">` + stars.join(" ")
+    + `</div>
+  </div>`
+
+  $("#skillTable > div:nth-child(" + (childrenLength - 1) + ")").after(mockElem);
+}
+
+function removeSkill(elem) {
+  elem.parentNode.parentNode.parentNode.removeChild(elem.parentNode.parentNode)
+  
+  let children = $("#skillTable").children()
+  let childrenLength = children.length
+  for (let index = 0; index < childrenLength; index++) {
+    if (index !== 0 && index !== childrenLength - 1) {
+      const element = children[index].querySelector('div.serial')
+      element.innerText = ('0' + index).slice(-2) 
+    }
+  }
 }
--- js/theme.js
+++ js/theme.js
@@ -20,5 +20,33 @@
 		}
 	}
 	navbarFixed();
-
+	
+	if (localStorage.getItem("isColorful") === "true") {
+		$("html").addClass("colorful-bg")
+		$("nav").addClass("colorful-bg")
+		$(".header_area .navbar .nav .nav-item:hover .nav-link").addClass("colorful-menu-font")
+		$(".header_area .navbar .nav .nav-item.active .nav-link").addClass("colorful-menu-font")
+		$(".colorful-menu").addClass("colorful-menu-font")
+		$("body").addClass("colorful-font")
+		$("footer").addClass("colorful-bg")
+	} else {
+		$("html").removeClass("colorful-bg")
+		$("nav").removeClass("colorful-bg")
+		$(".header_area .navbar .nav .nav-item:hover .nav-link").removeClass("colorful-menu-font")
+		$(".header_area .navbar .nav .nav-item.active .nav-link").removeClass("colorful-menu-font")
+		$(".colorful-menu").removeClass("colorful-menu-font")
+		$("body").removeClass("colorful-font")
+		$("footer").removeClass("colorful-bg")
+	}
 })(jQuery);
+
+
+function toggleColorfulMode() {
+	$("html").toggleClass("colorful-bg")
+	$("nav").toggleClass("colorful-bg")
+	$(".header_area .navbar .nav .nav-item:hover .nav-link").toggleClass("colorful-menu-font")
+	$(".header_area .navbar .nav .nav-item.active .nav-link").toggleClass("colorful-menu-font")
+	$("body").toggleClass("colorful-font")
+	$("footer").toggleClass("colorful-bg")
+	localStorage.setItem("isColorful", $("html").hasClass("colorful-bg"))
+}
